f47cdd00a59529f9c0da170a47f6e6e2040a9cbd
leandro <carolino.leandro@gmail.com>
2020-07-11 19:48:48 +0200

    feat(navbar)added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SignupComponent } from './components/signup/signup.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     SignupComponent,
     SettingsComponent,
     PageNotFoundComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
